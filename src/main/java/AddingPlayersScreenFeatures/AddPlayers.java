275be52f7f98459b130edbf7cc90e899244542bf
Maylayloo <milosz.l-24@wp.pl>
2021-08-06 00:39:48 +0200

    when name is null, tmp - nothing happens (user will get feedback)

--- src/main/java/AddingPlayersScreenFeatures/AddPlayers.java
+++ src/main/java/AddingPlayersScreenFeatures/AddPlayers.java
@@ -1,5 +1,6 @@
 package AddingPlayersScreenFeatures;
 
+import Arrays.ArrayGettingNicknamesToString;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.scene.layout.AnchorPane;
@@ -12,34 +13,48 @@ public class AddPlayers {
 
     public void add(AnchorPane container, JFXTextField txtField, JFXButton b1)
     {
-        ArrayNicknames.nicknames.add(new JFXButton());
 
 
-        ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setStyle("-fx-background-color:  #e9eaf0; -fx-background-radius: 20; -jfx-button-type: RAISED; -fx-font-size: 16");
+       if (txtField.getText() == null)
+       {
+           System.out.println("not happening bro");
+       }
+       else
+       {
+           ArrayNicknames.nicknames.add(new JFXButton());
 
-        container.getChildren().add(ArrayNicknames.nicknames.get(NicknamesArrayIndex.index));
 
-        new SetNamesLayout().setLayout(ArrayNicknames.nicknames, NicknamesArrayIndex.index, StartingLayoutNicknamesButtonsArray.layoutX, StartingLayoutNicknamesButtonsArray.layoutY);
+           ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setStyle("-fx-background-color:  #e9eaf0; -fx-background-radius: 20; -jfx-button-type: RAISED; -fx-font-size: 16");
 
-        ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setPrefSize(212, 37);
-        ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setRipplerFill(Paint.valueOf("#e9eaf0"));
+           container.getChildren().add(ArrayNicknames.nicknames.get(NicknamesArrayIndex.index));
 
-        ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setText(txtField.getText());
-        txtField.setText(null);
+           new SetNamesLayout().setLayout(ArrayNicknames.nicknames, NicknamesArrayIndex.index, StartingLayoutNicknamesButtonsArray.layoutX, StartingLayoutNicknamesButtonsArray.layoutY);
 
-        NicknamesArrayIndex.index += 1;
-        StartingLayoutNicknamesButtonsArray.layoutX += 250;
+           ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setPrefSize(212, 37);
+           ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setRipplerFill(Paint.valueOf("#e9eaf0"));
 
-        if (ArrayNicknames.nicknames.size() % 5 == 0)
-        {
-            StartingLayoutNicknamesButtonsArray.layoutY += 60;
-            StartingLayoutNicknamesButtonsArray.layoutX = 32;
-        }
+           ArrayNicknames.nicknames.get(NicknamesArrayIndex.index).setText(txtField.getText());
 
-        if (ArrayNicknames.nicknames.size() == 45)
-        {
-            b1.setDisable(true);
-            txtField.setDisable(true);
-        }
+           new SaveNicknames().addNameToArray(ArrayGettingNicknamesToString.nicknamesInString, txtField.getText());
+
+           txtField.setText(null);
+
+           NicknamesArrayIndex.index += 1;
+           StartingLayoutNicknamesButtonsArray.layoutX += 250;
+
+           if (ArrayNicknames.nicknames.size() % 5 == 0)
+           {
+               StartingLayoutNicknamesButtonsArray.layoutY += 60;
+               StartingLayoutNicknamesButtonsArray.layoutX = 32;
+           }
+
+           if (ArrayNicknames.nicknames.size() == 45)
+           {
+               b1.setDisable(true);
+               txtField.setDisable(true);
+           }
+       }
+
+
     }
 }
